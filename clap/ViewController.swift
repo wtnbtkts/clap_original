706a6fac4b50f8bff48e3ebd0238ba1cc694db84
Takatoshi Watanabe <abacus.taka-1125@i.softbank.jp>
2015-06-14 15:31:03 +0900

    first commit

---
+++ clap/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  clap
+//
+//  Created by Takatoshi Watanabe on 2015/06/14.
+//  Copyright (c) 2015年 渡辺 貴俊. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, UIPickerViewDelegate,AVAudioPlayerDelegate {
+    @IBOutlet var clapPickerView: UIPickerView!
+    var audioPlayer : AVAudioPlayer!
+    var soundCount:Int=0
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //再生する音源のURLを生成.
+        let soundFilePath = NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
+        let fileURL : NSURL = NSURL(fileURLWithPath:soundFilePath)!
+        
+        
+        //AVAudioPlayerのインスタンス化.!
+        audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+    }
+    func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
+            return 1
+    }
+    //いくつ選択肢をつくるか
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+            return 10
+            
+    }
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)
+        -> String! {
+        return "\(row+1)回"
+    }
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        soundCount = row
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func playButton(){
+            audioPlayer.numberOfLoops = soundCount
+            audioPlayer.play()
+    }
+    
+    
+}
+
+
