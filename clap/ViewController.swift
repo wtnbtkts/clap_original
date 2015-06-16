1b5c0d573364bc1e1cb9478bc2686308e83d6bf6
Takatoshi Watanabe <abacus.taka-1125@i.softbank.jp>
2015-06-17 01:24:01 +0900

    second commit

--- clap/ViewController.swift
+++ clap/ViewController.swift
@@ -15,49 +15,77 @@ class ViewController: UIViewController, UIPickerViewDelegate,AVAudioPlayerDelega
     var soundCount:Int=0
     
     
+    var stations = ["わずか、1問正解！", "まあ、2問正解！", "おお、3問正解！", "さすが、4問正解！"]
+    //var filenames: [String!] = ["dog", "cat", "rain" ,"wave"]
     
     override func viewDidLoad() {
+        
+        
+        
         super.viewDidLoad()
+        
         //再生する音源のURLを生成.
-        let soundFilePath = NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
+        let soundFilePath = NSBundle.mainBundle().pathForResource("answer", ofType: "mp3")!
         let fileURL : NSURL = NSURL(fileURLWithPath:soundFilePath)!
         
         
         //AVAudioPlayerのインスタンス化.!
         audioPlayer = AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+
     }
+    
+    /*
+    func playerWithFilename(filename: String!) -> AVAudioPlayer {
+    let soundFilePath = NSBundle.mainBundle().pathForResource(filename, ofType: "mp3")!
+    let fileURL : NSURL = NSURL(fileURLWithPath:soundFilePath)!
+    
+    
+    //AVAudioPlayerのインスタンス化.!
+    return AVAudioPlayer(contentsOfURL: fileURL, error: nil)
+    }
+    */
+    
+    
+    /*
     func numberOfComponentsInPickerView(pickerView: UIPickerView!) -> Int {
-            return 1
+    return 1
     }
     //いくつ選択肢をつくるか
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
-            return 10
-            
+    return 10
+    
     }
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)
-        -> String! {
-        return "\(row+1)回"
+    -> String! {
+    return "\(row+1)回"
     }
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
+    soundCount = row
+    }*/
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return stations.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        return stations[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         soundCount = row
     }
     
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     @IBAction func playButton(){
-            audioPlayer.numberOfLoops = soundCount
-            audioPlayer.play()
+        //audioPlayer = playerWithFilename(filenames)
+        audioPlayer.numberOfLoops = soundCount
+        audioPlayer.play()
     }
     
     
 }
-
-
